a290d08ef0878dc00a7b977388edae4ce09290eb
CYUBAHIRO MWUMVANEZA YVES <45140079+yvesyule@users.noreply.github.com>
2023-11-21 09:12:48 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook data science and Ecosystem are summerized.
+
+# Some of the popular language data scientist use are:
+# - Python
+# - R
+# - SQL
+
+# Some of the commonly used libraries used by data scientist include:
+# - pandas
+# - matplotlib
+# - ggplot
+# - numpy
+# - tensorflow
+# - seaborn
+# 
+
+# |Data Science Tools|
+# |------------------|
+# |Jupyter Notebook|
+# |Pandas|
+# |GitHub|
+# |Python|
+# |Apache Hadoop|
+# |Apache Spark|
+# |etc|
+
+# ## Arthmetic expression 
+# ##### Examples: 
+# 
+# - Addition +
+# - Multiplication *
+# - Division /
+# - Substraction -
+# - Equal ==
+# - Not Equal !=
+# - Exponential **
+# - Modulus %
+
+# In[2]:
+
+
+# Arthmetic expression 
+
+a = 8
+b = 2
+# Add numbers with python 
+
+print (a + b)
+
+# Multiplying numbers with python 
+
+print (a * b)
+
+
+# In[9]:
+
+
+# Minutes to hours with python
+
+a = int(input("Inter time in minutes"))
+b = float(a/60)
+print (a , "minutes. Equal to " , b, "hours")
+
+
+# ## Objectives 
+# The objectiove of this notebook was:
+# 1. to complete the final assignments about data science tools
+# 2. To practice Jupyter Notebook and GitHub
+# 3. dive into data science world
+
+# ## Author 
+# 
+# **Yves Yule**
+
+# In[ ]:
+
+
+
+
